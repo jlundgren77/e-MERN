17e86bf9cad4ff494aaf92f0226d6a39f1723781
Jeff <jlundgren77@gmail.com>
2021-05-08 16:30:10 -0400

    adds items to purchase

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -6,6 +6,14 @@ const Home = () => (
     <h1>Home Page Needs Some Love</h1>
     <p>Buy Some Merch From Me</p>
     <p>And don't forget to tip your bartender</p>
+    <div>
+      my items are going here
+      <ul>
+        <li>item1</li>
+        <li>item2</li>
+        <li>item3</li>
+      </ul>
+    </div>
   </Layout>
 );
 export default Home;
