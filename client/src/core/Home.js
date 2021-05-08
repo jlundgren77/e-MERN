78a1fdf30cd8ad5760df6bc6b1ca2fc8cf779d39
Jeff <jlundgren77@gmail.com>
2021-05-08 16:22:41 -0400

    adds text to home page

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -3,6 +3,9 @@ import Layout from './Layout';
 const Home = () => (
   <Layout title='Home Page' description='Node React Ecommerce App'>
     ...home
+    <h1>Home Page Needs Some Love</h1>
+    <p>Buy Some Merch From Me</p>
+    <p>And don't forget to tip your bartender</p>
   </Layout>
 );
 export default Home;
